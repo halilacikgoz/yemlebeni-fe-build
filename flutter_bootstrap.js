7fefa3b82e47a54724aee96401f3ffe92b37b3f6
halilacikgoz <halilacikgoz189@gmail.com>
2026-03-28 23:35:59 +0300

    Deploy Flutter Web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1401861745" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1420805483" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
